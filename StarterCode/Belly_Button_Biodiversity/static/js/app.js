7d90afac84577d44199d26a9b7003ac27804bfec
summerjets <mmcauley14@qub.ac.uk>
2019-03-15 22:38:37 +0000

    debug updates

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -51,16 +51,23 @@ function buildCharts(sample) {
       return ((a.sample_values < b.sample_values) ? -1 : ((a.sample_values == b.sample_values) ? 0:1));
   });
 
+  var otu_ids_l = new Array();
+  var otu_labels_l = new Array();
+  var sample_values_l = new Array();
 
   // seperate lists again
   for (var k = 0; k < combined_sample.length; k++) {
-    otu_ids_l[k] = combined_sample[k].otu_ids;
-    otu_labels_l[k] = combined_sample[k].otu_labels;
-    sample_values_l[k] = combined_sample[k].otu_labels;
-    console.log(otu_ids_l)
-    // looks like it's 
+    //otu_ids_l[k] = combined_sample[k].otu_ids;
+    otu_ids_l.push(combined_sample[k].otu_ids);
+    //otu_labels_l[k] = combined_sample[k].otu_labels;
+    otu_labels_l.push(combined_sample[k].otu_labels);
+    //sample_values_l[k] = combined_sample[k].sample_values;
+    sample_values_l.push(combined_sample[k].sample_values);
+    // console.log(otu_ids_l)
+    // looks like it's populated now
   };
 
+console.log(otu_ids_l)
 
     // @TODO: Build a Bubble Chart using the sample data
   var bubbletrace1 = {
@@ -76,34 +83,48 @@ function buildCharts(sample) {
 var data = [{
   x: [1999, 2000, 2001, 2002],
   y: [10, 15, 13, 17],
-  type: 'scatter'
 }];
-  
+
   var layout = {
     title: 'Marker Size',
     showlegend: false,
     height: 600,
     width: 600
   };
-  console.log(data)
-  Plotly.newPlot('bubble', data, layout);
+  //console.log(data)
+  //Plotly.newPlot('bubble', data, layout);
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+  var otu_ids_s = otu_ids_l.slice(1,10);
+  var otu_labels_s = otu_labels_l.slice(1,10);
+  var sample_values_s = sample_values_l.slice(1,10);
+console.log("otu_labels_s")
+  console.log(otu_labels_s)
+  console.log("sample_values_s")
+  console.log(sample_values_s)
+
+
+  var data2 = [{
+    values: sample_values_s,
+    labels: otu_labels_s,
+    type: "pie"
+  }];
+
+// var data2 = [{
+//   values: [19, 26, 55],
+//   labels: ['Residential', 'Non-Residential', 'Utility'],
+//   type: 'pie'
+// }];
+
+  var layout2 = {
+    height: 400,
+    width: 500
+  };
+
+  Plotly.newPlot('pie', data2, layout2);
 });
-  // var otu_ids_s = otu_ids_l.slice(10);
-  // var otu_labels_s = otu_labels_l.slice(10);
-  // var sample_values_s = sample_values_l.slice(10);
-  // var data2 = [{
-  //   values: sample_values_s,
-  //   labels: otu_labels_s,
-  //   type: "pie"
-  // }];
-  // var layout2 = {
-  //   height: 400,
-  //   width: 500
-  // };
-  // Plotly.newPlot('pie', data2, layout2);
 
 };
 
